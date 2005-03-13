69ac95e827d7606629b2058793eb8ce994dfab31
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2005-03-13 14:07:20 +0000

    update - wh
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@11427 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/isWithinInterval.R
+++ R/isWithinInterval.R
@@ -1,11 +1,13 @@
 isWithinInterval = function(x, start, end) {
-  if(!(is.numeric(x) && is.vector(x)))
-    stop("'x' must be a numeric vector.")
-  if(!(is.numeric(start) && is.vector(start) && is.numeric(end) && is.vector(end) &&
-       length(end)==length(start) && identical(names(start), names(end))))
-    stop("'start' and 'end' must be numeric vectors of the same length and with same names.")
   n = length(x)
   m = length(start)
+
+  if(!(is.numeric(x) && is.vector(x) && n>0))
+    stop("'x' must be a numeric vector of length > 0.")
+  
+  if(!(is.numeric(start) && is.vector(start) && is.numeric(end) && is.vector(end) &&
+       length(end)==m && identical(names(start), names(end)) && m>0))
+    stop("'start' and 'end' must be numeric vectors of the same length > 0, and with same names.")
 
   mx  = matrix(x, nrow=n, ncol=m)
   res = ((mx >= matrix(start, nrow=n, ncol=m, byrow=TRUE)) &
