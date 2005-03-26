550d86acdac01d31a7d55f62a0f58509f23c58c4
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2005-03-26 21:12:47 +0000

    sparse matrix implementation of isWithinInterval.R - wh
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@11611 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/isWithinInterval.R
+++ R/isWithinInterval.R
@@ -5,14 +5,24 @@ isWithinInterval = function(x, start, end) {
   if(!(is.numeric(x) && is.vector(x) && n>0))
     stop("'x' must be a numeric vector of length > 0.")
   
-  if(!(is.numeric(start) && is.vector(start) && is.numeric(end) && is.vector(end) &&
-       length(end)==m && identical(names(start), names(end)) && m>0))
-    stop("'start' and 'end' must be numeric vectors of the same length > 0, and with same names.")
+  if(!(is.numeric(start) && is.vector(start) && is.numeric(end) &&
+       is.vector(end) && length(end)==m &&
+       identical(names(start), names(end)) && m>0))
+    stop(paste("'start' and 'end' must be numeric vectors of the",
+         "same length > 0, and with same names."))
 
-  mx  = matrix(x, nrow=n, ncol=m)
-  res = ((mx >= matrix(start, nrow=n, ncol=m, byrow=TRUE)) &
-         (mx <= matrix(end,   nrow=n, ncol=m, byrow=TRUE)))
+  if(!require("SparseM")) {
+    warning(paste("Package 'SparseM' could not be loaded, reverting",
+        "to normal matrices (this maybe rather memory inefficient)."))
+   
+    mx  = matrix(x, nrow=n, ncol=m)
+    res = ((mx >= matrix(start, nrow=n, ncol=m, byrow=TRUE)) &
+           (mx <= matrix(end,   nrow=n, ncol=m, byrow=TRUE)))
+  } else {
 
+    
+  }
+  
   rownames(res)=names(x)
   colnames(res)=names(start)
   return(res)
