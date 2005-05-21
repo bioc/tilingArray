972e30e05c0ba2243b19ab56e740edf4aad13c50
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2005-05-21 23:32:18 +0000

    a script to visualize and qualitatively assess different normalization methods
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@12671 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/scripts/assessNorm.R
@@ -0,0 +1,85 @@
+library("Biobase")
+library("RColorBrewer")
+
+if(!exists("a")) {
+  cat("Loading a.rda\n")
+  load("a.rda")
+}
+if(!exists("probeAnno")) {
+  cat("Loading probeAnno.rda\n")
+  load("probeAnno.rda")
+}
+if(!exists("xn")) {
+  cat("Loading xn.rda\n")
+  load("seg-polyA-050521/xn.rda")
+}
+
+if(!exists("refSigThresh")) {
+  chrstr = paste(rep(1:17, each=2),
+    rep(c("+", "-"), 17), sep=".")
+  allPM = unique(unlist(lapply(chrstr, function(chr)
+    get(paste(chr, "index", sep="."), probeAnno))))
+  refSigThresh = quantile(refSig[allPM], probs=0.05)
+}
+cat("refSigThresh=", signif(refSigThresh, 3), "\n")
+
+jref = which(a$NucleicAcid == "DNA")
+stopifnot(length(jref)==3)
+
+fn = c("05_04_27_2xpolyA_NAP3.cel.gz",
+      "05_04_26_2xpolyA_NAP2.cel.gz",
+      "05_04_20_2xpolyA_NAP_2to1.cel.gz")
+
+## UFD2: Chr 4-, 118-122000
+sta = probeAnno$"4.-.start"
+ind = probeAnno$"4.-.index"
+sel = (sta>=115000 & sta<=125000)
+## sel = (sta>=116000 & sta<=116400)
+
+isel = ind[sel]
+xsel = sta[sel]
+
+y1 = rowMeans(log(exprs(a)[isel, jref], 2))
+y2 = rowMeans(log(exprs(a)[isel, fn], 2))
+y3 = rowMeans(exprs(xn)[isel,  ])
+y4 = y3
+y4[refSig[isel]<refSigThresh] = NA
+ysel = cbind(y1, y2, y2-y1, y3, y4)
+
+colnames(ysel) = c("DNA", "unnormalized", "normalization method 1",
+      "normalization method 2","normalization method 3")
+
+graphics.off()
+x11(width=8, height=12)
+par(mfrow=c(ncol(ysel), 1))
+rgnf = range(rms)
+cols = colorRamp(brewer.pal(11, "Spectral"))((rms[isel]-rgnf[1])/(rgnf[2]-rgnf[1])) / 256
+cols = rgb(cols[,1], cols[,2], cols[,3])
+
+for(i in 1:ncol(ysel))
+  plot(xsel, ysel[,i], pch=20, col=cols, xlab="coordinates (chr 4-)", ylab="signal",
+       main=colnames(ysel)[i])
+
+dev.copy(pdf, file="assessNorm.pdf", width=8, height=12); dev.off()
+
+## x11(width=6, height=6)
+## plot(ysel[, c("RNA new norm.", "RNA old norm.")], pch=17, col=c("black", "red")[1+badProbes[isel]])
+
+criterion = function(x) {
+  x  = x[!is.na(x)]
+  dd = abs(x[-(1:4)]-x[(0:3)-length(x)]) / IQR(x, na.rm=TRUE)
+  quantile(dd, probs=c(0.5))
+}
+
+sel = (sta>=100000 & sta<=140000)
+isel = ind[sel]
+xsel = sta[sel]
+y1 = rowMeans(log(exprs(a)[isel, jref], 2))
+y2 = rowMeans(log(exprs(a)[isel, fn], 2))
+y3 = rowMeans(exprs(xn)[isel,  ])
+y4 = y3
+y4[refSig[isel]<refSigThresh] = NA
+ysel = cbind(y1, y2, y2-y1, y3, y4)
+print(apply(ysel[order(xsel), ], 2, criterion))
+
+ 
