df62b670d642a518b1e39d364ea0dba098dcc13d
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2005-05-21 23:31:18 +0000

    normalize.R now uses background coreection; segment.R now processes multiple hybeSets; also removed the "baseline" subtraction.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@12670 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/scripts/normalize.R
@@ -0,0 +1,91 @@
+library("vsn")
+source("~/madman/Rpacks/vsn/R/vsn.R")
+
+library("genefilter")
+if(!exists("a"))load("a.rda")
+if(!exists("probeAnno"))load("probeAnno.rda")
+
+what = "polyA2"
+
+hybeSets = list(
+  "polyA2" = c("05_04_27_2xpolyA_NAP3.cel.gz",
+    "05_04_26_2xpolyA_NAP2.cel.gz",
+    "05_04_20_2xpolyA_NAP_2to1.cel.gz"),
+  "tot" = c("050409_totcDNA_14ug_no52.cel.gz",
+    "030505_totcDNA_15ug_affy.cel.gz"),
+  ##"050415_totcDNA_20ug_Affy11.cel.gz"
+  "tot2" = c("050331_totcDNA_15ug_S96.cel.gz",
+    "050411_totcDNA_20ug_affy.cel.gz",
+    "050415_totcDNA_20ug_Affy11.cel.gz"),
+  "dir" = "050507_dirRNA_10ug_F1.cel.gz")[-4]
+
+outdir = c("polyA2" = "seg-polyA-050521",
+           "tot"    = "seg-tot-050521",
+           "tot2"   = "seg-tot2-050521",
+           "dir"    = "seg-dir-050521")[-4]
+
+stopifnot(length(outdir)==length(hybeSets))
+          
+##
+## DNA hybes
+## 
+jref = which(a$NucleicAcid == "DNA")
+stopifnot(length(jref)==3)
+
+if(!exists("allPM")) {
+  ## 1. select PM probes
+  chrstr = paste(rep(1:17, each=2), 
+    rep(c("+", "-"), 17), sep=".")
+  allPM = unique(unlist(lapply(chrstr, function(chr)
+    get(paste(chr, "index", sep="."), probeAnno))))
+  
+  ## 2. select intergenic probes
+  allPM = allPM[ probeAnno$probeReverse$no_feature[allPM]=="no" &
+    probeAnno$probeDirect$no_feature[allPM]=="no" ]
+  
+  cat("Selected", length(allPM), "intergenic PM probes.\n")
+}
+
+if(!exists("strata")) {
+  ## DNA-normalization factor:
+  refSig    = rowMeans(log(exprs(a)[, jref, drop=FALSE], 2))
+  refSigPM  = refSig[allPM]
+  
+  nrStrata  = 20
+  quants    = quantile(refSig, probs=seq(0, 1, length=nrStrata+1))
+  quants[1] = quants[1]-1
+  strata    = cut(refSigPM, quants)
+}
+
+for(hs in seq(along=hybeSets)) {
+  cat(names(hybeSets)[hs], "\n")
+  fn = hybeSets[[hs]]
+  
+  x.bg  = tapply(refSigPM, strata, median)
+  y.bg  = matrix(NA, nrow=length(x.bg), ncol=length(fn))
+  bgfun = vector(mode="list", length=length(fn))
+  colnames(y.bg) = names(bgfun) = fn
+  for(f in fn) {
+    y.bg[,f]   = tapply(log(exprs(a)[allPM, f], 2), strata, shorth)
+    bgfun[[f]] = approxfun(x.bg, y.bg[,f], rule=2)
+  }    
+  matplot(x.bg, y.bg, pch=15+seq(along=fn))
+  px = seq(5, 13, by=0.1)
+  for(bf in bgfun)
+    lines(px, bf(px))
+  
+  yn = matrix(NA, nrow=nrow(exprs(a)), ncol=length(fn))
+  colnames(yn) = fn
+  for(f in fn)
+    yn[, f] = (exprs(a)[, f] - 2^bgfun[[f]](refSig) ) / 2^refSig
+
+  ## vsn with strata? Not sure whether this is useful
+  ## vsr = vsn(yn, lts.quantile=0.95, strata=as.integer(strata), subsample=1e5)
+  
+  ## vsn without strata
+  xn = vsn(yn, lts.quantile=0.95, subsample=2e5)
+  exprs(xn)    = exprs(xn)/log(2)
+  phenoData(xn) = phenoData=phenoData(a)[fn, ]
+
+  save(xn, refSig, file=file.path(outdir[hs], "xn.rda"), compress=TRUE)
+}
--- inst/scripts/segment.R
+++ inst/scripts/segment.R
@@ -1,5 +1,4 @@
 options(warn=0)
-## library("prada")  ### for shorth
 library("tilingArray")
 
 nrBasesPerSeg = 1400
@@ -8,75 +7,61 @@ options(error=recover, warn=2)
 if(!exists("probeAnno"))
   load("probeAnno.rda")
 
-## if(!exists("x")) 
-##   load("x.rda")
-
-what = c("polyA2", "tot")[1]
-switch(what,
-  "polyA2" = {
-    outdir = "seg-polyA-050518"
-  },
-  "tot"    = {
-    outdir = "seg-tot-050518"
-  },
-  stop(paste("Bummer:", what))
-)
-load(file.path(outdir, "xn.rda"))
-lxj = exprs(xn)
-stopifnot(ncol(lxj) %in% c(2,3))
+outdirList = c("polyA2" = "seg-polyA-050521",
+  "tot"    = "seg-tot-050521",
+  "tot2"   = "seg-tot2-050521")
 
 hybeType = "Reverse"
-
-#igvalues = lxj[get(paste("probe", hybeType, sep=""), envir=probeAnno)$no_feature == "no", ]
-#baseline = shorth(igvalues, na.rm=TRUE)
-#if(interactive())
-#  hist(igvalues, col="orange", breaks=100, main=sprintf("baseline=%3.1f", baseline))
-
 chrstr = paste(rep(1:17, each=2),
-             rep(c("+", "-"), 17), sep=".")
+               rep(c("+", "-"), 17), sep=".")
 
-## Output Directory
-if(!file.exists(outdir))
-  stop(paste("Directory '", outdir, "' does not exist.", sep=""))
-if(!file.info(outdir)$isdir)
-  stop(paste("'", outdir, "' must be a directory.", sep=""))
+for(outdir in outdirList) {
+  if(!file.exists(outdir))
+    stop(paste("Directory '", outdir, "' does not exist.", sep=""))
+  if(!file.info(outdir)$isdir)
+    stop(paste("'", outdir, "' must be a directory.", sep=""))
 
-for(chr in chrstr) {
-  datfn = file.path(outdir, paste(chr, ".rda", sep=""))
-  if(!file.exists(datfn)) {
-    cat("working on", datfn, "\n")
-    ## Lock it
-    con = file(datfn, open="wt")
-    writeLines(date(), con)
-    close(con)
+  for(ichr in seq(along=chrstr)) {
+    chr = chrstr[ichr]
+    if(ichr==1) {
+      load(file.path(outdir, "xn.rda"))
+      lxj = exprs(xn)
+      stopifnot(ncol(lxj) %in% c(2,3))
+    }
+
+    datfn = file.path(outdir, paste(chr, ".rda", sep=""))
+    if(!file.exists(datfn)) {
+      cat("working on", datfn, "\n")
+      ## Lock it
+      con = file(datfn, open="wt")
+      writeLines(date(), con)
+      close(con)
     
-    ## get data
-    ind = get(paste(chr, "index", sep="."), probeAnno)
-    sta = get(paste(chr, "start", sep="."), probeAnno)
-    uni = get(paste(chr, "unique", sep="."), probeAnno)
+      ## get data
+      ind = get(paste(chr, "index", sep="."), probeAnno)
+      sta = get(paste(chr, "start", sep="."), probeAnno)
+      end = get(paste(chr, "end", sep="."), probeAnno)
+      uni = get(paste(chr, "unique", sep="."), probeAnno)
+      yraw = lxj[ind, ]
 
-    yraw = lxj[ind, ] #  -baseline
-
-    ## use approx
-    dat = list(x=seq(min(sta), max(sta), by=8))
-    dat$y        = apply(yraw, 2, function(y) { approx(sta, y, xout=dat$x, ties=mean)$y } )
-    dat$xunique  = as.logical(round(approx(sta, uni, xout=dat$x, ties=mean)$y))
-    dat$start    = sta
-    dat$yraw     = yraw
-    dat$unique   = uni
-    ## dat$baseline = baseline
+      ## use approx
+      mids = (sta+end)/2
+      dat  = list(start = sta, end = end, yraw = yraw, unique = uni,
+        x=seq(min(mids), max(mids), by=8))
+      dat$y        = apply(yraw, 2, function(y) { approx(mids, y, xout=dat$x, ties=mean)$y } )
+      dat$xunique  = as.logical(round(approx(mids, uni, xout=dat$x, ties=mean)$y))
      
-    ## see plotFeatSize: the longest structure CDS in yeast is
-    ## 15k bases long, corresponding to about 2000 consecutive probes
-    maxk  = 1500
-    maxcp = round(diff(range(dat$x)) / nrBasesPerSeg)
-    
-    seg = findSegments(dat$y, maxk=maxk, maxcp=maxcp, verbose=99)
+      ## see plotFeatSize: the longest structure CDS in yeast is
+      ## 15k bases long, corresponding to about 2000 consecutive probes
+      maxk  = 1500
+      maxcp = round(diff(range(dat$x)) / nrBasesPerSeg)
       
-    save(seg, dat, file=datfn, compress=TRUE)
-  } else {
-    cat(datfn, "exists, skipping.\n")
-  } ## if
-} # for chr
-
+      seg = findSegments(dat$y, maxk=maxk, maxcp=maxcp, verbose=99)
+      
+      save(seg, dat, file=datfn, compress=TRUE)
+    } else {
+      cat(datfn, "exists, skipping.\n")
+    } ## if
+  } # for chr
+} ## for outdir
 
