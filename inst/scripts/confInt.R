c07cb876fa0c94fc5f93b8c5f5383a1e7550320e
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2005-07-30 21:48:42 +0000

    seed for a function that will hopefully calculate confidence intervals for changepoints
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@13421 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/scripts/confInt.R
@@ -0,0 +1,91 @@
+library(tilingArray)
+options(error=recover)
+
+l = c(10,  17, 3, 27, 10, 13)
+m = c( 2, 2.2, 4, 0,   2, 1.4)
+S = length(l)
+
+maxk = 30
+
+cp = 1+cumsum(l)
+s  = 0.1
+
+y = unlist(lapply(seq(along=l), function(i) {
+  rnorm(l[i], mean=m[i], sd=s)
+}))
+
+estcp = findSegments(y, maxcp=S, maxk=maxk, verbose=0)$th[S,]
+
+plot(y)
+abline(v=estcp, col="red")
+
+sqr = function(x){x*x}
+
+## what would it cost to move the changepoints?
+w = function(y, cp, alpha=0.95) {
+  
+  ## create of a list whose elements are the data values within each segment
+  cp1 = c(1, cp)
+  segY = lapply(seq(along=cp), function(k) {
+    y[cp1[k]:(cp1[k+1]-1)]
+  })
+  segSum = sapply(segY, sum)
+  segSS  = mapply(function(x,s) sum(sqr(x-s/length(x))), segY, segSum)
+  segLen = listLen(segY)
+  
+  ## estimated variance (unbiased version)
+  s2 = sum(segSS) / (length(y)-length(cp))
+
+  ## By how much can the sum of squares increase?
+  ## Denote by L the negative log-likelihood, by SS the sum of
+  ## squared residuals, and by n the number of residuals, then
+  ##   Delta L  = n/2 log (Delta SS/n)   <=>
+  ##   Delta SS = n * exp(2L/n)
+  ##
+  ## We assume that the likelihood ratio statistic follows
+  ##   -2 log(L_rest/L_abs) ~ Chi(1)^2
+  ## implying a critical value for -log(L_rest/L_abs)
+  ##   of qchisq(alpha, df=1)/2
+  ## Hence the critical value for SS is
+  ##   n * exp(qchisq(alpha, df=1)/n)
+  
+  qchisq(alpha, df=1)
+ 
+ dsl = dsr = numeric(as.numeric(NA), ncol=length(cp)-1)
+ 
+ for(j in 1:ncol(dsl)) {
+
+   th  = cp[j]
+   thl = if(j>=2) {cp[j-1]} else {1}
+   thr = cp[j+1]
+
+   yj  = y[thl:(thr-1)]
+   sj  = (segSS[j]+segSS[j+1])
+   
+   for(i in 1:(th-thl)) {
+     ## extend to the left by i
+     yl  = sum(y[th-(1:i)])
+     m1l = (segSum[j]   - yl) / (segLen[j]  -i)
+     m2l = (segSum[j+1] + yl) / (segLen[j+1]+i)
+     nml = rep(c(m1l, m2l), c(th-thl-i, thr-th+i))
+     ssl = sum(sqr( yj-nml ))
+     dsl[i,j] = ssl - sj
+   }
+   
+   for(i in 1:(thr-th)) {
+     ## extend to the right by i
+     yr  = sum(y[th+(0:(i-1))])
+     m1r = (segSum[j]   + yr) / (segLen[j]   +i)
+     m2r = (segSum[j+1] - yr) / (segLen[j+1] -i)
+     nmr = rep(c(m1r, m2r), c(th-thl+i, thr-th-i))
+     ssr = sum(sqr( yj-nmr ))
+
+     dsr[i,j] = ssr - sj
+   }
+ }
+ stopifnot(all(dsr>=0), all(dsl>=0))
+}
+
+
+cat("w\n")
+w(y, estcp)
