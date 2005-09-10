61edc75d460f61b9ce349e75226916f03c2ce926
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2005-09-10 09:11:05 +0000

    added clustering of the windows
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@14027 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/lowTranscribedRegions.R
+++ inst/scripts/lowTranscribedRegions.R
@@ -1,5 +1,5 @@
 
-nrSelect  = 250
+nrSelect  = 400
 
 library("tilingArray")
 library("arrayMagic")
@@ -50,36 +50,39 @@ movingAverage = function(x1, y1, x2, y2, windowWidth, windowStep, nmin=4) {
 }
 
 
-####
+##------------------------------------------------------------
+## Find the windows
+##------------------------------------------------------------
 windowWidth = 100
 windowStep  = 25
 chrs = 1:16
 
 if(!exists("mavs")) {
-mavs = vector(mode="list", length=length(chrs))
-for(chr in chrs) {
-  dat1 = get(paste(chr, "+.dat", sep="."), envir=get(rt))
-  dat2 = get(paste(chr, "-.dat", sep="."), envir=get(rt))
-  
-  x1  = (dat1$start+dat1$end)/2
-  x2  = (dat2$start+dat2$end)/2
-  y1  = rowMeans(dat1$y)
-  y2  = rowMeans(dat2$y)
-  
-  mavs[[chr]] = movingAverage(x1, y1, x2, y2, windowWidth, windowStep)
-  
-  plot(x1, y1, pch=".", col="grey", main=paste(chr, sep="."))
-  points(x2, y2, pch=".", col="grey")
-  points(ma$xout, ma$yout, pch=".", col="orange")
-}
+  mavs = vector(mode="list", length=length(chrs))
+  for(chr in chrs) {
+    dat1 = get(paste(chr, "+.dat", sep="."), envir=get(rt))
+    dat2 = get(paste(chr, "-.dat", sep="."), envir=get(rt))
+    
+    x1  = (dat1$start+dat1$end)/2
+    x2  = (dat2$start+dat2$end)/2
+    y1  = rowMeans(dat1$y)
+    y2  = rowMeans(dat2$y)
+    
+    mavs[[chr]] = ma = movingAverage(x1, y1, x2, y2, windowWidth, windowStep)
+    
+    if(!TRUE){
+      plot(x1, y1, pch=".", col="grey", main=paste(chr, sep="."))
+      points(x2, y2, pch=".", col="grey")
+      points(ma$xout, ma$yout, pch=".", col="orange")
+    }
+  }
 }
 
 allValues = unlist(lapply(mavs, "[[", "yout"))
 thresh    = sort(allValues)[nrSelect]
 
-res = data.frame(chr=integer(nrSelect),
-  midpoint=integer(nrSelect),
-  plot=I(character(nrSelect)))
+resRaw = data.frame(chr=integer(nrSelect),
+  midpoint=integer(nrSelect))
 
 
 alongChromWidth = 15e3
@@ -101,13 +104,59 @@ for(chr in chrs) {
     mids = mavs[[chr]]$xout[wh]
     idx = j + (1:length(mids))
     
-    res[idx, "chr"] = chr
-    res[idx, "midpoint"] = mids
-    hyper = mapCoord2Plot(chr, mids, mids)
-    res[idx, "plot"] = sprintf("<a href=\"viz/%s.jpg\" target=\"new\">%s</a>",
-         hyper, hyper)
+    resRaw[idx, "chr"] = chr
+    resRaw[idx, "midpoint"] = mids
     j = j + length(mids)
   }
 }
+stopifnot(j==nrSelect)
+          
+##------------------------------------------------------------
+## Cluster them
+##------------------------------------------------------------
+hc = hclust(dist(resRaw$chr*1e8+resRaw$midpoint), method="single")
+ct = cutree(hc, h=1000)
+stopifnot(length(ct)==nrSelect)
 
-write.htmltable(res, outfile, title=rt, formatNumeric=function(x) paste(as.integer(x)))
+nrClust = ct[length(ct)]
+
+resClust = data.frame(no=1:nrClust,
+  chr=integer(nrClust),
+  firstMidpoint=integer(nrClust),
+  lastMidpoint=integer(nrClust),
+  nrWindows=integer(nrClust),
+  feature=I(character(nrClust)),
+  plot=I(character(nrClust)))
+
+sgff = gff[ gff$feature %in% c("telomere", "centromere"), ]
+
+for(i in 1:nrClust) {
+  wh = which(ct==i)
+  stopifnot(length(wh)>=1)
+  chr = unique(resRaw[wh, "chr"])
+  stopifnot(length(chr)==1)
+  resClust[i, "chr"] = chr
+  resClust[i, "firstMidpoint"] = resRaw[wh[1], "midpoint"]
+  resClust[i, "lastMidpoint"]  = resRaw[wh[length(wh)], "midpoint"]
+  resClust[i, "nrWindows"]   = length(wh)
+
+  ## Annotate telomeres, centromeres
+  d = pmin(abs( sgff$chr*1e8+sgff$start -
+                    (chr*1e8+resClust$firstMidpoint[i])),
+           abs( sgff$chr*1e8+sgff$end -
+                    (chr*1e8+resClust$lastMidpoint[i])))
+
+  whd = which(d<2000)
+  stopifnot(length(whd)<=1)
+  if(length(whd)==1)
+    resClust[i, "feature"] = sgff$Name[whd]
+  
+  ## Plot
+  mmm = mean(resRaw[wh, "midpoint"])
+  hyper = mapCoord2Plot(chr, mmm, mmm)
+  resClust[i, "plot"] = sprintf("<a href=\"viz/%s.jpg\" target=\"new\">%s</a>",
+         hyper, hyper)
+}
+
+                          
+write.htmltable(resClust, outfile, title=rt, formatNumeric=function(x) paste(as.integer(x)))
