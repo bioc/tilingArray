fc4d49d0eb6ccd276452fada7ec0307d46c47ca3
Jorn Todling <toedling@ebi.ac.uk>
2005-10-09 10:24:25 +0000

    corrected transcription status of uORFs, to hinder them from interfering with the discovery of novel transcripts
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@14404 bc3139a8-67e5-0310-9ffc-ced21a209358

--- data/yeastFeatures.R
+++ data/yeastFeatures.R
@@ -3,7 +3,7 @@
 yeastFeatures = data.frame(
   isTranscribed = I(c("gene"=TRUE,"CDS"=TRUE,"CDS_dubious"=TRUE,"intron"=TRUE,"repeat_region"=TRUE,
   "ncRNA"=TRUE, "nc_primary_transcript"=TRUE,"tRNA"=TRUE,"snRNA"=TRUE, "snoRNA"=TRUE, "rRNA"=TRUE,
-  "uORF"=TRUE,"transposable_element"=TRUE,"transposable_element_gene"=TRUE,"pseudogene"=TRUE,
+  "uORF"=FALSE,"transposable_element"=TRUE,"transposable_element_gene"=TRUE,"pseudogene"=TRUE,
   "nucleotide_match"=FALSE,"chromosome"=FALSE,"insertion"=FALSE, "region"=FALSE, "repeat_family"=FALSE, 
   "TF_binding_site"=FALSE,"binding_site"=FALSE,"ARS"=FALSE,"centromere"=FALSE, "telomere"=FALSE)))
 
