bf67d26c651976aa4b8ce4300f4018238e8c7560
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2006-03-19 10:55:10 +0000

    model selection is now part of segmentation.Rnw script
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@16734 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/testAICBIC.R
+++ inst/scripts/testAICBIC.R
@@ -1,6 +1,6 @@
 library(tilingArray)
 
-if(!exists("segwi"))load("1.+.rda")
+if(!exists("segBig"))load("~/segBig.RData")
 
 ## AIC = -2 logLik + 2*npar
 ## BIC = -2 logLik + log(ndat)*npar
@@ -14,16 +14,32 @@ penLL = function(ll, npar, ndat, what)
 plotPenLL = function(s) {
   cols = brewer.pal(3, "Set1")
   ll = logLik(s)
-  plot(ll, type="l", col=cols[1], ylab="(penalized) log likelihood", xlab="S")
+  ylim = ll[c(50, length(ll))]
+  plot(ll, type="l", col=cols[1], ylab="(penalized) log likelihood", xlab="S", ylim=ylim)
   lines(penLL(ll, npar=2*(1:length(ll)), ndat=nrow(s@y), what="AIC"), col=cols[2])
-  lines(penLL(ll, npar=2*(1:length(ll)), ndat=nrow(s@y), what="BIC"), col=cols[3])
+  bic = penLL(ll, npar=2*(1:length(ll)), ndat=nrow(s@y), what="BIC")
+  lines(bic, col=cols[3])
+
+  lenChr= s@x[length(s@x)]
+  nrSegChoice = round(lenChr/1500)
+  optBIC = mean(which.max(bic))
+  x0= c(nrSegChoice, optBIC)
+  segments(x0=x0, x1=x0, y0=ylim[1]+diff(ylim)*0.05, y1=ylim[1]+diff(ylim)*0.95,
+          col=c("#707070", cols[3]), lty=2)
+  
+  legend(0, ylim[2], expression(LL, tilde(LL)[AIC], tilde(LL)[BIC]),
+         xjust=0, yjust=1, col=cols, lwd=1)
 }
 
+pdf(file="testAICBIC.pdf", width=4.5, height=4.5)
+par(mai=c(1,1,0.01,0.01))
+plotPenLL(segBig)
+dev.off()
 
 stop()
 
-## using one segment: concordance of logLik.lm, my pedestrian formula,
-## and the output of 'segment'
+## Test the concordance of logLik.lm, my pedestrian formula,
+## and the output of 'segment', for the trivial case of only one segment
 
 n  = seq(10, 100, by=10)
 ll = matrix(as.numeric(NA), nrow=length(n), ncol=3)
@@ -43,5 +59,3 @@ for(i in seq(along=n)) {
 pairs(ll)
 stopifnot(all(diff(t(ll))<1e-8))
 
-## using multiple segments
- seg = segment(y, maxseg=10, maxk=length(y))
