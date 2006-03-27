2c106faf690a54c4d38f44439d1d867274bd1975
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2006-03-27 20:32:41 +0000

    trimmed excess comments
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@16859 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGeneric.R
+++ R/AllGeneric.R
@@ -1,25 +1,8 @@
-##
-## S3 generic functions "confint" and "logLik" are defined in package "stats",
-##   here we try to keep consistent with that:
-##
-## > confint
-## function (object, parm, level = 0.95, ...)
-## UseMethod("confint")
-## <environment: namespace:stats>
-##
-## > logLik
-## function (object, ...)
-## UseMethod("logLik") 
-## <environment: namespace:stats>
-##
-
 if(!isGeneric("confint"))
   setGeneric("confint", useAsDefault=confint)
-##  setGeneric("confint", function(object, parm, level = 0.95, ...) standardGeneric("confint"))
 
 if(!isGeneric("logLik"))
   setGeneric("logLik", useAsDefault=logLik)
-##  setGeneric("logLik", function(object, ...) standardGeneric("logLik"))
 
 if(!isGeneric("plot"))
   setGeneric("plot", useAsDefault=plot)
