e6d8c171cbf5dafd99743a4b91eb36c0c9b1da13
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2006-03-30 19:15:26 +0000

    lwd=2
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@16919 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotPenLL.R
+++ R/plotPenLL.R
@@ -10,7 +10,7 @@ plotPenLL = function(seg) {
   optBIC = mean(which.max(lls[,3]))
   x0= c(nrSegChoice, optBIC)
   segments(x0=x0, x1=x0, y0=ylim[1]+diff(ylim)*0.05, y1=ylim[1]+diff(ylim)*0.95,
-          col=c("#707070", cols[3]), lty=2)
+          col=c("#707070", cols[3]), lty=2, lwd=2)
   legend(0, ylim[2], expression(log~L, log~tilde(L)[AIC], log~tilde(L)[BIC]),
          xjust=0, yjust=1, col=cols, lwd=1, pch=16)
 }
