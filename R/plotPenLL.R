1246411ad0a3253a790bcdc96f7c8f827f99cd68
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2006-03-30 19:43:05 +0000

    better colors of dashed bars
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@16921 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotPenLL.R
+++ R/plotPenLL.R
@@ -1,16 +1,16 @@
-plotPenLL = function(seg) {
+plotPenLL = function(seg, extrabar=numeric(0)) {
   cols = brewer.pal(3, "Set1")
   lls = cbind(logLik(seg), logLik(seg, penalty="AIC"), logLik(seg, penalty="BIC"))
 
   ylim = c(min(apply(lls, 2, quantile, probs=0.2)), max(lls))
   matplot(lls, type="b", col=cols, ylab="(penalized) log likelihood", lty=1, pch=16,xlab="S", ylim=ylim)
 
-  lenChr= seg@x[length(seg@x)]
-  nrSegChoice = round(lenChr/1500)
   optBIC = mean(which.max(lls[,3]))
-  x0= c(nrSegChoice, optBIC)
+  
+  x0 = c(optBIC, extrabar)
+  names(x0)[1] = cols[3]
   segments(x0=x0, x1=x0, y0=ylim[1]+diff(ylim)*0.05, y1=ylim[1]+diff(ylim)*0.95,
-          col=c("#707070", cols[3]), lty=2, lwd=2)
+          col=names(x0), lty=2, lwd=2)
   legend(0, ylim[2], expression(log~L, log~tilde(L)[AIC], log~tilde(L)[BIC]),
          xjust=0, yjust=1, col=cols, lwd=1, pch=16)
 }
