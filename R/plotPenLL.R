62be15993df35f6f9c4edd983eadd18280e77f78
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2006-05-21 22:24:08 +0000

    changes to modelSelect plot, in response to reviewer comments
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@18169 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotPenLL.R
+++ R/plotPenLL.R
@@ -1,9 +1,10 @@
-plotPenLL = function(seg, extrabar=numeric(0)) {
+plotPenLL = function(seg, extrabar=numeric(0), type="b", pch=16, lwd=1) {
   cols = brewer.pal(3, "Set1")
   lls = cbind(logLik(seg), logLik(seg, penalty="AIC"), logLik(seg, penalty="BIC"))
 
   ylim = c(min(apply(lls, 2, quantile, probs=0.2)), max(lls))
-  matplot(lls, type="b", col=cols, ylab="(penalized) log likelihood", lty=1, pch=16,xlab="S", ylim=ylim)
+  matplot(lls, type=type, col=cols, ylab="(penalized) log likelihood",
+          lty=1, lwd=lwd, pch=pch, xlab="S", ylim=ylim)
 
   optBIC = mean(which.max(lls[,3]))
   
@@ -12,5 +13,5 @@ plotPenLL = function(seg, extrabar=numeric(0)) {
   segments(x0=x0, x1=x0, y0=ylim[1]+diff(ylim)*0.05, y1=ylim[1]+diff(ylim)*0.95,
           col=names(x0), lty=2, lwd=2)
   legend(0, ylim[2], expression(log~L, log~tilde(L)[AIC], log~tilde(L)[BIC]),
-         xjust=0, yjust=1, col=cols, lwd=1, pch=16)
+         xjust=0, yjust=1, col=cols, lwd=lwd, pch=pch)
 }
