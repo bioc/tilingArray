38e24bbf07f8cc0cb72d9e427a9f6f98a6c83994
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2006-09-23 19:24:27 +0000

    Updates to the scripts: use PMindex, BGindex instead of the cumbersome constructions of before
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@20061 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/marina.R
+++ inst/scripts/marina.R
@@ -3,13 +3,17 @@
 ##
 ## Please specify the parameters in the following lines:
 
-## Name of RNA hybes. Multiple files can be separated by comma.
+## Name of RNA and of DNA hybes. Multiple files can be separated by comma.
 rna = "050208_mRNA30minx4_RH6.cel, 050209_mRNAx4_30min_re-hybe_RH6.cel, 050218_polyA-RNA_RH6_4x15'.cel"
+dna = "09_11_04_S96_genDNA_16hrs_45C_noDMSO.cel, 041119_S96genDNA_re-hybe.cel, 041120_S96genDNA_re-hybe.cel"
 
-## Directory Path to the RNA hybes
-pathrna = /ebi/research/huber/Projects/tilingArray/Celfiles
+## Directory Path to the CEL files
+celpath = "/home/huber/Marina/Celfiles"
+#pathrna = "/ebi/research/huber/Projects/tilingArray/Celfiles"
 
-## Name of DNA hybes
-dna = 
-
+  
 ## Now we are done with setting the parameters, and can run the code
+## ------------------------------------------------------------------------
+
+## Step 1: read the cel files "a"
+a <- readCel2eSet(c(rna, dna), path=celpath)
