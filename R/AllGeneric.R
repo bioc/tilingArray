694a58f249fc97766dbdf8192fedaad5fe7ad74a
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2007-03-27 21:10:12 +0000

    removed unnecessary PDF file, and obsolete commented out code in AllGeneric.R
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tilingArray@23497 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGeneric.R
+++ R/AllGeneric.R
@@ -1,8 +1,3 @@
 setGeneric("confint", useAsDefault=confint)
 setGeneric("logLik", useAsDefault=logLik)
 
-##if(!isGeneric("plot"))
-##  setGeneric("plot", useAsDefault=plot)
-
-##if(!isGeneric("show"))
-##  setGeneric("show", useAsDefault=show)
